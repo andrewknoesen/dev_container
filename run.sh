a1ad3d09fb196c1daf714dc804943a5ecfb3609d
Andrew Knoesen <andrew@spatialedge.co.za>
2025-07-22 21:16:55 +0200

    Completed nvim setup

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 set -e
 docker rm -f dev-container
 docker build -t dev-container .
-# docker run --name dev-container -it -v $HOME/.ssh:/root/.ssh:ro -v ./workdir:/workdir -v ./nvim:/root/.config/nvim dev-container
+docker run --name dev-container -it -v $HOME/.ssh:/root/.ssh:ro -v ./workdir:/workdir -v ./nvim:/root/.config/nvim dev-container
 
-docker run --name dev-container -it -v $HOME/.ssh:/root/.ssh:ro -v ./workdir:/workdir dev-container
+# docker run --name dev-container -it -v $HOME/.ssh:/root/.ssh:ro -v ./workdir:/workdir dev-container
